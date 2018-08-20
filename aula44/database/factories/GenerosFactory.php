89d7c71d09c69f777e515d63609cddd746b992c4
Marcelo Diament <39604367+Marcelo-Diament@users.noreply.github.com>
2018-08-20 20:32:16 -0300

    a44-ex-aula
    Aula: 44
    Tema: Laravel IX (Seeders e Factories)
    Observação: criação de Seeder e Factory exemplo

---
+++ aula44/database/factories/GenerosFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Genre::class, function (Faker $faker) {
+
+        return [
+            'name' => $faker->word,
+            'ranking' => $faker->unique->numberBetween(30,99),
+            'active' => $faker->boolean($chanceOfGettingTrue = 80),
+        ];
+
+});
--- aula44/database/seeds/GeneroSeeder.php
+++ aula44/database/seeds/GeneroSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Genre;
+
 class GeneroSeeder extends Seeder
 {
     /**
@@ -11,6 +13,60 @@ class GeneroSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        // $generos = Genero::all();
+        // $generos->delete();
+
+        // $genero = Genero::create('name'=>'Comédia','ranking'=>1)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Terror','ranking'=>2)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Drama','ranking'=>3)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Ação','ranking'=>4)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Ficção Científica','ranking'=>5)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Suspense','ranking'=>6)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Animação','ranking'=>7)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Aventura','ranking'=>8)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Documentário','ranking'=>9)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Infantil','ranking'=>10)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Fantasia','ranking'=>11)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // $genero = Genero::create('name'=>'Musical','ranking'=>2)                
+        // $genero->active = true;
+        // $genero->save();
+
+        // CRIANDO GENEROS PELO SEEDS DE ACORDO COM DEFINIÇÃO DA FACTORY
+        $generos = factory(Generos:class)->times(5)->create();
+
     }
 }
