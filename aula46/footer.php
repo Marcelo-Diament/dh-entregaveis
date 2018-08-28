1b593278274932a2153223405955b1f750d4371b
Marcelo-Diament <marcelodiament@gmail.com>
2018-08-28 08:07:57 -0300

    logos-3d-menores

--- aula46/footer.php
+++ aula46/footer.php
@@ -1,7 +1,7 @@
 		<div id="logos" class="desktop-only">
 		    <a id="cube" href="https://djament.com.br" title="Djament Comunicação" alt="Djament Comunicação" rel="external" target="_blank">
-		        <img src="https://djament.com.br/assets/img/logo-60x60.png" height="auto" width="auto"  id="front" alt="Djament" title="Djament">
-		        <img src="https://br.digitalhouse.com/wp-content/themes/dh/assets/img/icons/apple-icon-60x60.png" height="auto" width="auto" id="back" alt="Digital House" title="Digital House">
+		        <img src="https://djament.com.br/assets/img/logo-60x60.png" height="30px" width="30px"  id="front" alt="Djament" title="Djament">
+		        <img src="https://br.digitalhouse.com/wp-content/themes/dh/assets/img/icons/apple-icon-60x60.png" height="30px" width="30px" id="back" alt="Digital House" title="Digital House">
 		    </a>
 		</div>
 	</body>
